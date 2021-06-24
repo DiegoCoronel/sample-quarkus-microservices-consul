2572bffdcd1d45c0d92cafe0b0dbbf164401af6e
Diego Coronel <diego.coronel@fpf.br>
2021-06-24 08:54:18 -0400

    remover lombok

--- organization-service/src/main/java/pl/piomin/services/quarkus/organization/model/Employee.java
+++ organization-service/src/main/java/pl/piomin/services/quarkus/organization/model/Employee.java
@@ -1,16 +1,8 @@
 package pl.piomin.services.quarkus.organization.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
 public class Employee {
 
     private Long id;
@@ -18,4 +10,35 @@ public class Employee {
     private int age;
     private String position;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
